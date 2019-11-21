679c8e3bbd974bdbcf5a82db93bed61258317e8b
rahulvn389 <51837819+rahulvn389@users.noreply.github.com>
2019-11-21 14:44:17 -0800

    Update common.mk

--- modules/module_02/makefile/common.mk
+++ modules/module_02/makefile/common.mk
@@ -1,28 +1,4 @@
 
-runOnfpga_$(TARGET).xo: $(SRCDIR)/compute_score_fpga.cpp $(SRCDIR)/hls_stream_utils.h
-	v++ -c -g -t $(TARGET) -R 1 -k runOnfpga \
-		--platform xilinx_u200_xdma_201830_2 \
-		--save-temps \
-		--temp_dir ./temp_dir \
-		--report_dir ./report_dir \
-		--log_dir ./log_dir \
-		--profile_kernel data:all:all:all:all \
-		-I$(SRCDIR) \
-		-DPARALLELISATION=$(PF) \
-		$(SRCDIR)/compute_score_fpga.cpp \
-		-o ./runOnfpga_$(TARGET).xo
-
-runOnfpga_$(TARGET).xclbin: ./runOnfpga_$(TARGET).xo
-	v++ -l -g -t $(TARGET) -R 1 \
-		--platform xilinx_u200_xdma_201830_2 \
-		--temp_dir ./temp_dir \
-		--report_dir ./report_dir \
-		--log_dir ./log_dir \
-		--profile_kernel data:all:all:all:all \
-		-I$(SRCDIR) \
-		./runOnfpga_$(TARGET).xo \
-		-o ./runOnfpga_$(TARGET).xclbin 
-
 host: $(SRCDIR)/*.cpp $(SRCDIR)/*.c $(SRCDIR)/*.h
 	mkdir -p $(BUILDDIR)
 	g++ -D__USE_XOPEN2K8 -D__USE_XOPEN2K8 \
