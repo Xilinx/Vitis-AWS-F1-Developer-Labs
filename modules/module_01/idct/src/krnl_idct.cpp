c3e7bcc5151defd2ec5c00f85a0e4c15fb63bec4
ravicho <44479645+ravicho@users.noreply.github.com>
2019-12-18 18:29:25 -0800

    Updated to original state

--- modules/module_01/idct/src/krnl_idct.cpp
+++ modules/module_01/idct/src/krnl_idct.cpp
@@ -319,14 +319,14 @@ void krnl_idct_dataflow(const ap_int<512> *block,
 			ap_int<512> *voutp, 
 			int ignore_dc, 
 			unsigned int blocks) {
-  #pragma HLS DATAFLOW
+  //#pragma HLS DATAFLOW
 
   hls::stream<int512_t> iblock;
   hls::stream<uint512_t> iq;
   hls::stream<int512_t> ivoutp;
-  //#pragma  HLS stream variable=iblock depth=512
-  //#pragma  HLS stream variable=iq     depth=2
-  //#pragma  HLS stream variable=ivoutp depth=512
+  #pragma  HLS stream variable=iblock depth=512
+  #pragma  HLS stream variable=iq     depth=2
+  #pragma  HLS stream variable=ivoutp depth=512
 
 
   read_blocks<uint512_t>(q, iq, 1);
